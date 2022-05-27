8ceeb30fd68cd43e965fd71e5f286ec3bc9da943
Vince R <r2d2fan20@outlook.com>
2022-05-27 15:48:16 -0500

    init

---
+++ app/App.svelte
@@ -0,0 +1,30 @@
+<script lang="typescript">
+  let message: string = "Blank Svelte Native App";
+</script>
+
+<page>
+  <actionBar title="Rithem" />
+  <gridLayout>
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label
+      class="info"
+      horizontalAlignment="center"
+      verticalAlignment="middle"
+      textWrap="true"
+    >
+      <formattedString>
+        <span class="fas" text="&#xf135;" />
+        <span text=" {message}" />
+      </formattedString>
+    </label>
+  </gridLayout>
+</page>
+
+<style>
+  .info .fas {
+    color: #3a53ff;
+  }
+  .info {
+    font-size: 20;
+  }
+</style>
