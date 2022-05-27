878153ea6e1624faae6d8c0ad20755a1c077f601
Vince R <r2d2fan20@outlook.com>
2022-05-27 18:30:37 -0500

    a

--- app/App.svelte
+++ app/App.svelte
@@ -1,30 +1,50 @@
 <script lang="typescript">
-  let message: string = "Blank Svelte Native App";
+	let message: string = "Blank Svelte Native App";
 </script>
 
 <page>
-  <actionBar title="Rithem" />
-  <gridLayout>
-    <!-- svelte-ignore a11y-label-has-associated-control -->
-    <label
-      class="info"
-      horizontalAlignment="center"
-      verticalAlignment="middle"
-      textWrap="true"
-    >
-      <formattedString>
-        <span class="fas" text="&#xf135;" />
-        <span text=" {message}" />
-      </formattedString>
-    </label>
-  </gridLayout>
+	<!-- <actionBar title="Rithem" /> -->
+	<actionBar title="ActionBar Title">
+		<gridLayout>
+			<label
+				text="RithemBoards"
+				fontSize="24"
+				verticalAlignment="center"
+			/>
+			<button class="material-icons action-bar-button">leaderboard</button
+			>
+		</gridLayout>
+	</actionBar>
+
+	<gridLayout>
+		<!-- svelte-ignore a11y-label-has-associated-control -->
+		<label
+			class="info"
+			horizontalAlignment="center"
+			verticalAlignment="middle"
+			textWrap="true"
+		>
+			<formattedString>
+				<span class="fas" text="&#xf135;" />
+				<span text=" {message}" />
+			</formattedString>
+		</label>
+	</gridLayout>
 </page>
 
 <style>
-  .info .fas {
-    color: #3a53ff;
-  }
-  .info {
-    font-size: 20;
-  }
+	.info .fas {
+		color: #3a53ff;
+	}
+	.info {
+		font-size: 20;
+	}
+
+	.action-bar-button {
+		font-size: 24;
+		background: red;
+		width: 90px;
+		height: 90px;
+		border-radius: 80px;
+	}
 </style>
