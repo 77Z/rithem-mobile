3502c2df3d4182f8d763320227fd50100239969e
Vince R <r2d2fan20@outlook.com>
2022-05-27 22:26:02 -0500

    what

--- app/Leaderboard.svelte
+++ app/Leaderboard.svelte
@@ -1,3 +1,20 @@
+<script>
+	import { goBack } from "svelte-native";
+</script>
+
 <page>
-	<label text="Main" fontSize="50" verticalAlignment="center" />
+	<actionBar title="ActionBar Title">
+		<gridLayout>
+			<navigationButton on:tap={goBack} class="material-icons"
+				>arrow_back</navigationButton
+			>
+			<label
+				text="RithemBoards"
+				fontSize="24"
+				verticalAlignment="center"
+			/>
+			<!-- <button class="material-icons action-bar-button">search</button> -->
+		</gridLayout>
+	</actionBar>
+	<label text="Leaderboard" fontSize="50" verticalAlignment="center" />
 </page>
