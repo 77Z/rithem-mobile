70621501cfd0aceb128b15326855b2d9b7a5c64b
Vince R <r2d2fan20@outlook.com>
2022-05-28 14:13:21 -0500

    ha

--- app/App.svelte
+++ app/App.svelte
@@ -7,7 +7,7 @@
 
 <page>
 	<!-- <actionBar title="Rithem" /> -->
-	<actionBar title="ActionBar Title">
+	<!-- <actionBar title="ActionBar Title">
 		<gridLayout>
 			<button
 				class="material-icons action-bar-button"
@@ -19,25 +19,42 @@
 				fontSize="24"
 				verticalAlignment="center"
 			/>
-			<!-- <button class="material-icons action-bar-button">search</button> -->
 		</gridLayout>
+	</actionBar> -->
+
+	<actionBar title="RithemBoards">
+		<gridLayout>
+			<button
+				class="material-icons action-bar-button"
+				on:tap={() => navigate({ page: Leaderboard })}
+				>leaderboard</button
+			>
+			<label
+				text="RithemBoards"
+				fontSize="24"
+				verticalAlignment="center"
+			/>
+		</gridLayout>
+		<button
+			android.position="actionBar"
+			class="material-icons action-bar-button"
+			fontFamily="MaterialIcons">more_vert</button
+		>
+		<button
+			android.position="actionBar"
+			class="material-icons action-bar-button"
+			fontFamily="MaterialIcons">account_circle</button
+		>
 	</actionBar>
 
-	<gridLayout>
-		<!-- svelte-ignore a11y-label-has-associated-control -->
-		<!-- <a href="yes">Yo</a> -->
-		<label text="Main" fontSize="50" verticalAlignment="center" />
-	</gridLayout>
+	<scrollView>
+		<stackLayout>
+			<label>Welcome to the offical Rithem leaderboards app</label>
+		</stackLayout>
+	</scrollView>
 </page>
 
 <style>
-	.info .fas {
-		color: #3a53ff;
-	}
-	.info {
-		font-size: 20;
-	}
-
 	.action-bar-button {
 		font-size: 24;
 		/* background: red; */
